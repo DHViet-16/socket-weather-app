6e94441d317cceefc87107fec31cb75cb568f3c1
quyen6 <quyenthai.060603@gmail.com>
2025-03-27 17:00:04 +0700

    insert script file

--- Client/script.js
+++ Client/script.js
@@ -40,7 +40,6 @@ socket.on("weatherData", (data) => {
     socket.emit("error", "Không thể lấy dữ liệu thời tiết!");
   }
 
-
   updateWeatherInfo(data);
 });
 //
@@ -63,7 +62,6 @@ function updateWeatherInfo(data) {
       forecastday: [
         {
           day: { maxtemp_c, mintemp_c },
-       
         },
       ],
     },
@@ -83,6 +81,119 @@ function updateWeatherInfo(data) {
   pressure.innerHTML = `<strong>${pressure_mb}</strong> Mb`;
   vision.innerHTML = `<strong>${vis_km}</strong> Km`;
   updateForecastsInfo(data);
+}
+async function updateForecastsInfo(city) {
+  const forecastsData = await getFetchData(city);
+
+  if (!forecastsData || !forecastsData.forecast) {
+    console.log("Dữ liệu dự báo không hợp lệ.");
+    return;
+  }
+
+  const forecastDays = forecastsData.forecast.forecastday;
+  const now = new Date();
+  let closestForecast = null;
+  let count = 0;
+  // Tìm thời điểm gần nhất trong tương lai
+  for (const forecastWeather of forecastDays) {
+    for (const item of forecastWeather.hour) {
+      const forecastTime = new Date(item.time);
+      if (
+        forecastTime >= now &&
+        (!closestForecast || forecastTime < closestForecast)
+      ) {
+        closestForecast = forecastTime;
+      }
+    }
+  }
+
+  // Xóa nội dung cũ trước khi cập nhật
+  forecastItemsContainer.innerHTML = "";
+  forecastItemsTomorrowContainer.innerHTML = "";
+  // Hiển thị dự báo giờ gần nhất
+  forecastDays.forEach((item) => {
+    item.hour.forEach((forecastWeather) => {
+      const forecastTime = new Date(forecastWeather.time);
+      if (forecastTime >= closestForecast && count < 10) {
+        updateForecastsItems(forecastWeather);
+        count++;
+      }
+    });
+  });
+
+  // Lọc dự báo ngày mai
+  const timeTaken = "12:00";
+  const todayDate = new Date().toISOString().split("T")[0];
+
+  forecastDays.forEach((item) => {
+    item.hour.forEach((forecastWeather) => {
+      if (
+        forecastWeather.time.includes(timeTaken) &&
+        !forecastWeather.time.includes(todayDate)
+      ) {
+        updateForecastsTomorrowItems(forecastWeather);
+      }
+    });
+  });
+}
+//  DỰ BÁO THỜI TIẾT CÁC KHOẢNG THỜI GIAN TRONG NGÀY
+function updateForecastsItems(forecastWeather) {
+  const {
+    time,
+    temp_c,
+    condition: { icon },
+  } = forecastWeather;
+
+  const dateTaken = new Date(time);
+  const timeResult = dateTaken.toLocaleTimeString("vi-VN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  const forecastItem = `
+            <div class="forecast-item">
+          <h5 class="forecast-item-date regular-txt">${timeResult}</h5>
+          <img
+            src="https://${icon}"
+            class="forecast-item-img"
+          />
+          <h5 class="forecast-item-temp">${Math.round(temp_c)} °C</h5>
+        </div>
+  `;
+  forecastItemsContainer.insertAdjacentHTML("beforeend", forecastItem);
+}
+//  DỰ BÁO THỜI TIẾT NGÀY MAI
+function updateForecastsTomorrowItems(forecastWeather) {
+  const {
+    time,
+    temp_c,
+    condition: { icon },
+  } = forecastWeather;
+
+  const dateTaken = new Date(time);
+  const dateOption = {
+    day: "2-digit",
+
+    month: "short",
+    // hour: "2-digit",
+  };
+  const dateResult = dateTaken.toLocaleDateString("en-US", dateOption);
+
+  const forecastTomorrowItem = `
+        <div class="forecast-tomorrow-items">
+        <h5 class="forecast-tomorrow-items-date regular-txt">${dateResult}</h5>
+        <img
+          src="https://${icon}"
+          class="forecast-tomorrow-items-img"
+        />
+        <h5 class="forecast-item-temp">${Math.round(temp_c)} °C</h5>
+      </div>
+  `;
+  forecastItemsTomorrowContainer.insertAdjacentHTML(
+    "beforeend",
+    forecastTomorrowItem
+  );
 }
 
 // TÌM KIẾM THÀNH PHỐ KHÔNG CÓ SẼ HIỂN THỊ TRANG NOT FOUND
